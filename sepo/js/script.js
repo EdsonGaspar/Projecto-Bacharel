abb4bae68317a5a67b12a66cceb68ffc9905645e
Edson Gaspar <114083090+EdsonGaspar@users.noreply.github.com>
2023-07-17 18:19:50 +0100

    Criando Carrocel de imagens
    Carrocel de imagem não acabo

---
+++ sepo/js/script.js
@@ -0,0 +1,15 @@
+/*Criando Um carrocel de imagens*/
+const imgs = document.getElementById("img");
+const img = document.querySelector("#img img");
+
+let idx = 0;
+
+function carrocel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+    imgs.style.transform = 'translateX($(idx * 500)px)';
+}
+setInterval(carrocel, 1800);
